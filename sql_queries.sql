e3201adfc8535f0aee25b3bfc5d82e0c509a58c1
Rajesh Kalakoti <rajeshkalakoti@gmail.com>
2023-08-09 14:44:16 +0300

    sql query

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,4 +1,4 @@
--- Create a new UTF-8 `snippetbox` database.
+-- Create a new UTF-8 `snippetbox` database.. 
 CREATE DATABASE snippetbox CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Switch to using the `snippetbox` database.
 USE snippetbox;
